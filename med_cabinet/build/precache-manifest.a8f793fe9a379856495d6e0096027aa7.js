af31f681eeab8cee42c336ef097b0de3132b30e8
Vicky <vyue013@gmail.com>
2019-08-30 08:57:47 -0700

    deployed on netlify, working

---
+++ med_cabinet/build/precache-manifest.a8f793fe9a379856495d6e0096027aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24353d276207afa5f6904600b3e960b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508a04deb790dd160160",
+    "url": "/static/css/main.6867a04c.chunk.css"
+  },
+  {
+    "revision": "51eb6da66c4e9d515d39",
+    "url": "/static/js/2.1a1d422d.chunk.js"
+  },
+  {
+    "revision": "508a04deb790dd160160",
+    "url": "/static/js/main.238a345e.chunk.js"
+  },
+  {
+    "revision": "38e2b4efc54023e3f54c",
+    "url": "/static/js/runtime~main.968bd77c.js"
+  },
+  {
+    "revision": "021c535061eea64d503a9f27feef5cea",
+    "url": "/static/media/MedCabinetLogo.021c5350.png"
+  }
+]);
